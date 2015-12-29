6cc9ece8fc240c93aebfc1dfa1d0568af3bfca51
Abdulla Kaleem <kaleem@keepit.pro>
2015-12-29 18:18:12 +0500

    this too, nothing

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -46,7 +46,7 @@
                 <div class="tag-box tag-box-v7">
                             <h2>Publication name - 29/11/1992</h2>
                             <p>Et harum quidem rerum facilis est et expedita distinctio lorem ipsum dolor sit amet consectetur adipiscing elit. Ut non libero consectetur adipiscing elit ellentesque viverra vehicu. Et harum quidem rerum facilis est et expedita distinctio lorem ipsum dolor...</p>
-                            <a class="btn-more" href="/#">Read More +</a>
+                            <a class="btn-more" href="/publication">Read More +</a>
                 </div>
            </div>
             
@@ -54,7 +54,7 @@
                 <div class="tag-box tag-box-v7">
                             <h2>Primary Education Standards</h2>
                             <p>Et harum quidem rerum facilis est et expedita distinctio lorem ipsum dolor sit amet consectetur adipiscing elit. Ut non libero consectetur adipiscing elit ellentesque viverra vehicu. Et harum quidem rerum facilis est et expedita distinctio lorem ipsum dolor...</p>
-                            <a class="btn-more" href="/#">Read More +</a>
+                            <a class="btn-more" href="/publication">Read More +</a>
                 </div>
            </div>
             
@@ -77,7 +77,7 @@
             <div class="row">
                 
                 <div class="col-md-12 text-center">
-                    <a href="#" class="btn  btn-u text-uppercase form-control"> View All Publications</a>
+                    <a href="/publication" class="btn  btn-u text-uppercase form-control"> View All Publications</a>
                 </div>
             </div>
         </div>
